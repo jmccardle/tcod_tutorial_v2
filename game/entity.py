90272e8576c87d0c1063da9739b348a043620f5f
John McCardle <mccardle.john@gmail.com>
2025-07-30 11:21:13 -0400

    Part 2: The generic Entity, render functions, and the map (with Part 8/10 refactoring)

--- game/entity.py
+++ game/entity.py
@@ -1,4 +1,9 @@
-from typing import Tuple
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Tuple
+
+if TYPE_CHECKING:
+    import game.game_map
 
 
 class Entity:
@@ -6,11 +11,35 @@ class Entity:
     A generic object to represent players, enemies, items, etc.
     """
 
-    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+    # Part 8 refactoring prep: Will become Union[GameMap, Inventory] in Part 8
+    gamemap: game.game_map.GameMap
+
+    def __init__(
+        self,
+        gamemap: Optional[game.game_map.GameMap] = None,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+    ):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        if gamemap:
+            # If gamemap isn't provided now then it will be set later.
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
+
+    def place(self, x: int, y: int, gamemap: Optional[game.game_map.GameMap] = None) -> None:
+        """Place this entity at a new location. Handles moving across GameMaps."""
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"):  # Possibly uninitialized.
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
