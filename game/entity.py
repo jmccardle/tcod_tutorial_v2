b6b8137930f0f314e0d0829aeda4bd9f13d07de7
John McCardle <mccardle.john@gmail.com>
2025-07-31 08:22:00 -0400

    Part 5: Placing Enemies (with Part 8/10 refactoring)

--- game/entity.py
+++ game/entity.py
@@ -21,11 +21,15 @@ class Entity:
         y: int = 0,
         char: str = "?",
         color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unnamed>",
+        blocks_movement: bool = False,
     ):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
         if gamemap:
             # If gamemap isn't provided now then it will be set later.
             self.gamemap = gamemap
